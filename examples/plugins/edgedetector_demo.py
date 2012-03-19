5620657689f5f8388bed26e53b18b59135ded734
Guillaume Gay <guillaume@mitotic-machine.org>
2012-03-19 15:35:49 +0100

    first implementation of the ContrastSetter plugin

--- examples/plugins/edgedetector_demo.py
+++ examples/plugins/edgedetector_demo.py
@@ -3,6 +3,7 @@ from skimage import data
 from skloupe.viewers import ImageViewer
 from skloupe.plugins import EdgeDetector
 
+
 image = skimage.img_as_float(data.camera())
 view = ImageViewer(image)
 ed = EdgeDetector(view)
