efd5addd7569acf36812de50e8e4f42999a000ce
Guillaume Gay <guillaume@mitotic-machine.org>
2012-03-20 12:49:39 +0100

    First working version of the Contrast Setter plugin.
    There still is an issue with the colormap not updating correctly in
    the image window, but the interactions are there at least.
    	modified:   ../skloupe/plugins/contrastsetter.py

--- skloupe/plugins/contrastsetter.py
+++ skloupe/plugins/contrastsetter.py
@@ -25,11 +25,12 @@ class ContrastSetter(Plugin):
 
     def __init__(self, image_window):
 
-        figure, axes = plt.subplots(nrows=3, figsize=(6, 1))
+        figure, axes = plt.subplots(nrows=3, figsize=(6.5, 3))
+        
         ax_histo, ax_low, ax_high = axes
+        
         self.ax_histo = ax_histo
         Plugin.__init__(self, image_window, figure=figure)
-        
         hmin, hmax = dtype_range[self.image.dtype.type]
         if hmax > 255:
             bins = int(hmax - hmin)
@@ -41,6 +42,13 @@ class ContrastSetter(Plugin):
         low_value, high_value = self.bin_centers[[0, -1]]
         clip = low_value, high_value
 
+        hist_lines = ax_histo.step(self.bin_centers, self.hist, 
+                                   color = 'k', alpha = 1.)
+        self.ax_histo.set_xlim(low_value, high_value)
+        self.ax_histo.set_xticks([])
+        self.ax_histo.set_yticks([])
+
+
         self.slider_high = Slider(ax_high, clip, label='Maximum',
                                   value=high_value, on_release=self.update_image)
         self.slider_low = Slider(ax_low, clip, label='Minimum',
@@ -48,11 +56,8 @@ class ContrastSetter(Plugin):
 
         self.connect_event('key_press_event', self.on_key_press)
         self.connect_event('scroll_event', self.on_scroll)
-
         self.original_image = self.imgview.image.copy()
         self.update_image()
-        self.redraw()
-        
         print self.help
 
     @property
@@ -72,23 +77,22 @@ class ContrastSetter(Plugin):
         return self.slider_high.value
 
     def update_image(self, event = None):
+
         self.draw_colorbar()
-        self.draw_histogram()
         self.imgview.image = self.original_image.clip(self.low, self.high)
         self.imgview.redraw()
         self.redraw()
-
-
-    def draw_histogram(self):
-        hist_lines = self.ax_histo.step(self.bin_centers, self.hist, 
-                                        color = 'k', alpha = 0.5)
+        
     def draw_colorbar(self):
         self.colorbar = np.linspace(self.low, self.high,
                                     256).reshape((1,256))
         extent = (self.low, self.high,
                   self.ax_histo.axis()[2], self.ax_histo.axis()[3])
+        if len(self.ax_histo.images) > 0 :
+            del self.ax_histo.images[-1]
         self.ax_histo.imshow(self.colorbar, aspect = 'auto',
                              extent = extent)
+        
 
     def reset(self):
         self.slider_high.value = self.bin_centers
@@ -100,32 +104,41 @@ class ContrastSetter(Plugin):
         center = (self.high + self.low) / 2.
         span = self.high - self.low
         low = min(self.slider_low.value - span / 20.,
-                  self.slider.valmin)
+                  self.slider_low.valmin)
         high = max(self.slider_high.value + span / 20.,
                    self.slider_high.valmax)
-        
+        self.slider_low.value = low
+        self.slider_high.value = high
+        self.update_image()
+                
 
     def _restrict_bonds(self, event):
         if not event.inaxes: return
         center = (self.high + self.low) / 2.
         span = self.high - self.low
         low = min(self.slider_low.value + span / 20.,
-                  self.slider._high.value - span / 20.)
+                  self.slider_high.value - span / 20.)
         high = max(self.slider_high.value - span / 20.,
                    self.slider_low.value + span / 20.)
+        self.slider_low.value = low
+        self.slider_high.value = high
+        self.update_image()
+            
         
     def on_scroll(self, event):
         if not event.inaxes: return
         if event.button == 'up':
-            self._expand_bonds()
+            self._expand_bonds(event)
         elif event.button == 'down':
-            self._restrict_bonds()
+            self._restrict_bonds(event)
+
+            
 
     def on_key_press(self, event):
         if not event.inaxes: return
         elif event.key == '+':
-            self._expand_bonds()
+            self._expand_bonds(event)
         elif event.key == '-':
-            self._restrict_bonds()
+            self._restrict_bonds(event)
         elif event.key == 'r':
             self.reset()
