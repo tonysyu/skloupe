811802dd7ae34322a926b56f9482823aaff7555e
Guillaume Gay <guillaume@mitotic-machine.org>
2012-03-21 09:52:01 +0100

    Code cleaning - trying to be more PEP8 complient 	modified:   plugins/contrastsetter_demo.py 	modified:   ../skloupe/plugins/contrastsetter.py

--- examples/plugins/contrastsetter_demo.py
+++ examples/plugins/contrastsetter_demo.py
@@ -1,10 +1,8 @@
-import skimage
 from skimage import data
 from skloupe.viewers import ImageViewer
 from skloupe.plugins import ContrastSetter
 
-
-image = data.coins()#skimage.img_as_float(data.coins())
+image = data.coins()
 view = ImageViewer(image)
 cs = ContrastSetter(view)
 view.show()
--- skloupe/plugins/contrastsetter.py
+++ skloupe/plugins/contrastsetter.py
@@ -1,13 +1,10 @@
 import matplotlib.pyplot as plt
-from skimage.filter import canny
 from skimage.util.dtype import dtype_range
 from skimage.exposure import histogram
-
-import numpy as np
+from numpy import linspace
 
 from .base import Plugin
 from ..widgets.slider import Slider
-
 
 __all__ = ['ContrastSetter']
 
@@ -20,39 +17,39 @@ class ContrastSetter(Plugin):
     ----------
     image_window : ImageViewer instance.
         Window containing image used in measurement.
-    
+
     """
 
     def __init__(self, image_window):
-
         figure, axes = plt.subplots(nrows=3, figsize=(6.5, 3))
-        
         ax_histo, ax_low, ax_high = axes
-        
         self.ax_histo = ax_histo
+
         Plugin.__init__(self, image_window, figure=figure)
+
         hmin, hmax = dtype_range[self.image.dtype.type]
         if hmax > 255:
             bins = int(hmax - hmin)
         else:
             bins = 256
-        print bins
-        
         self.hist, self.bin_centers = histogram(self.image.data, bins)
         low_value, high_value = self.bin_centers[[0, -1]]
         clip = low_value, high_value
 
-        hist_lines = ax_histo.step(self.bin_centers, self.hist, 
-                                   color = 'k', alpha = 1.)
+        hist_lines = ax_histo.step(self.bin_centers, self.hist,
+                                   color = 'r', lw = 2, alpha = 1.)
         self.ax_histo.set_xlim(low_value, high_value)
         self.ax_histo.set_xticks([])
         self.ax_histo.set_yticks([])
 
-
         self.slider_high = Slider(ax_high, clip, label='Maximum',
-                                  value=high_value, on_release=self.update_image)
+                                  value=high_value,
+                                  on_release=self.update_image)
         self.slider_low = Slider(ax_low, clip, label='Minimum',
-                                  value=low_value, on_release=self.update_image)
+                                 value=low_value,
+                                 on_release=self.update_image)
+        self.slider_low.slidermax = self.slider_high
+        self.slider_high.slidermin = self.slider_low
 
         self.connect_event('key_press_event', self.on_key_press)
         self.connect_event('scroll_event', self.on_scroll)
@@ -77,22 +74,20 @@ class ContrastSetter(Plugin):
         return self.slider_high.value
 
     def update_image(self, event = None):
-
         self.draw_colorbar()
         self.imgview.image = self.original_image.clip(self.low, self.high)
         self.imgview.redraw()
         self.redraw()
         
     def draw_colorbar(self):
-        self.colorbar = np.linspace(self.low, self.high,
-                                    256).reshape((1,256))
+        self.colorbar = linspace(self.low, self.high,
+                                 256).reshape((1,256))
         extent = (self.low, self.high,
                   self.ax_histo.axis()[2], self.ax_histo.axis()[3])
         if len(self.ax_histo.images) > 0 :
             del self.ax_histo.images[-1]
         self.ax_histo.imshow(self.colorbar, aspect = 'auto',
                              extent = extent)
-        
 
     def reset(self):
         self.slider_high.value = self.bin_centers
@@ -110,7 +105,6 @@ class ContrastSetter(Plugin):
         self.slider_low.value = low
         self.slider_high.value = high
         self.update_image()
-                
 
     def _restrict_bonds(self, event):
         if not event.inaxes: return
@@ -123,7 +117,6 @@ class ContrastSetter(Plugin):
         self.slider_low.value = low
         self.slider_high.value = high
         self.update_image()
-            
         
     def on_scroll(self, event):
         if not event.inaxes: return
@@ -131,8 +124,6 @@ class ContrastSetter(Plugin):
             self._expand_bonds(event)
         elif event.button == 'down':
             self._restrict_bonds(event)
-
-            
 
     def on_key_press(self, event):
         if not event.inaxes: return
