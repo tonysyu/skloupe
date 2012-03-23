a265a649c4f4b9463af397e800363a5f46a91922
Tony S Yu <tsyu80@gmail.com>
2012-03-22 22:30:24 -0400

    Remove outdated comment

--- examples/plugins/lineprofile_demo.py
+++ examples/plugins/lineprofile_demo.py
@@ -3,9 +3,6 @@ from skloupe.viewers import ImageViewer
 from skloupe.plugins import LineProfile
 
 image = data.camera()
-# Note: Widget must be assigned to a variable so it isn't garbage collected
-# Maybe LineProfile should save a reference of itself in ImageWindow
-# and then clear itself when closed.
 view = ImageViewer(image)
 LineProfile(view, limits='dtype')
 view.show()
